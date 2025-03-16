27c54d6c2389d68450d80661eeb3536c65575378
adarsh-papana <2387894@cognizant.com>
2025-03-16 10:50:17 +0530

    Notification

--- Inventory_Management/Repository/I_NotificationRepository.cs
+++ Inventory_Management/Repository/I_NotificationRepository.cs
@@ -4,7 +4,8 @@ namespace DigitalBookstoreManagement.Repository
 {
     public interface I_NotificationRepository
     {
-        public Task AddNotification(string message);
-        public Task<IEnumerable<Notification>> GetAllNotifications();
+        public Task AddNotificationAsync(string message);
+        public Task<IEnumerable<Notification>> GetAllNotificationsAsync();
+        public Task AddorUpdateNotificationAsync(int bookId, string title, int inventoryId, int notifylimit);
     }
 }
