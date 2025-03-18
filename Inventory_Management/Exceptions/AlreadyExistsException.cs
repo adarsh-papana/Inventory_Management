56590853bad3f6ead8f3d5c3fef376ca887d195b
adarsh-papana <2387894@cognizant.com>
2025-03-18 18:01:14 +0530

    exceptions

---
+++ Inventory_Management/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,7 @@
+﻿namespace DigitalBookstoreManagement.Exceptions
+{
+    public class AlreadyExistsException : Exception
+    {
+        public AlreadyExistsException(string message) : base(message) { }
+    }
+}
---
+++ Inventory_Management/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DigitalBookstoreManagement.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+       
+    }
+}
